5893c4db3bd59590f02e6dff457bf03ed5edad95
Andres <90808055+AndresMedin@users.noreply.github.com>
2023-07-14 23:07:38 -0500

    Main.cpp

--- Part 1/Main.cpp
+++ Part 1/Main.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <string>
+#include "functions.h"
 
+using namespace std;
+
+int main() {
+    string infixExpression;
+    cout << "Enter an infix expression: ";
+    getline(cin, infixExpression);
+
+    if (!isBalancedParentheses(infixExpression)) {
+        cout << "Invalid expression: Unbalanced parentheses." << endl;
+        return 0;
+    }
+
+    string postfixExpression = infixToPostfix(infixExpression);
+    cout << "Postfix expression: " << postfixExpression << endl;
+
+    return 0;
+}
